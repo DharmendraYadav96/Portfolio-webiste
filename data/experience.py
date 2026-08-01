b53c8f63e03cf664187831224fc14dce86f0955e
DharmendraYadav96 <dharmydv96@gmail.com>
2026-08-01 22:08:51 +0530

    update

--- data/experience.py
+++ data/experience.py
@@ -21,13 +21,32 @@ experience = [
     },
 
     {
+        "icon":"bi bi-bar-chart-fill",
+    
+        "role":"Data Analyst",
+    
+        "company":"Hi-Tech Digital Solutions",
+    
+        "duration":"2024 - Present",
+    
+        "description":"Worked on aviation datasets, production analytics, reporting and process optimization using Excel, SQL and Python.",
+    
+        "skills":[
+                "Python",
+                "SQL",
+                "Excel",
+                "Analytics"
+        ]
+    },
+
+    {
         "icon":"bi bi-airplane-fill",
 
         "role":"Senior Aeronautical Analyst",
 
         "company":"Hi-Tech Digital Solutions",
 
-        "duration":"2022 - Present",
+        "duration":"2018 - 2024",
 
         "description":"Created and quality checked ICAO compliant aeronautical charts for airlines using Navblue production standards.",
 
@@ -37,25 +56,8 @@ experience = [
             "Aviation",
             "QA"
         ]
-    },
-
-    {
-        "icon":"bi bi-bar-chart-fill",
-
-        "role":"Data Analyst",
-
-        "company":"Hi-Tech Digital Solutions",
-
-        "duration":"2018 - 2022",
-
-        "description":"Worked on aviation datasets, production analytics, reporting and process optimization using Excel, SQL and Python.",
-
-        "skills":[
-            "Python",
-            "SQL",
-            "Excel",
-            "Analytics"
-        ]
     }
+
+    
 
 ]
